b0c6cbeae20cdc501a41b3fdbf249ac9919d898f
abinayaruthira <abiperione@gmail.com>
2016-12-28 11:35:52 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class prime
+{
+public static void main(String[] args)
+{
+int a;
+int num;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number");
+a=s.nextInt();
+for(i=2;i++;i<a)
+{
+if(a%i==0)
+{
+
+flag=1;
+
+}
+}
+if(flag==1)
+{
+System.out.println("It is prime number");
+}
+}
